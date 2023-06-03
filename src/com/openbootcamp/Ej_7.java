71960a6fc1dc7afb859ee43332795d80875f64bc
Norberto Andres Luna <betoluna89@gmail.com>
2023-06-03 19:15:25 -0300

    Hecho el Ej7

--- src/com/openbootcamp/Ej_7.java
+++ src/com/openbootcamp/Ej_7.java
@@ -4,5 +4,18 @@ package com.openbootcamp;
 // ejemplo). Si se dispara la excepción, mostraremos el mensaje "Esto no puede hacerse". Finalmente, mostraremos en cualquier caso: "Demo de código".
 
 public class Ej_7 {
+    public static void main(String[] args) {
 
+        int num = 5;
+        try {
+            dividePorCero(num);
+        } catch (ArithmeticException e) {
+            System.out.println("Esto no puede hacerse: " + e.getMessage());
+        } finally {
+            System.out.println("Demo de codigo");
+        }
+    }
+    public static void dividePorCero(int numero) throws ArithmeticException {
+        System.out.println(numero / 0);
+    }
 }
