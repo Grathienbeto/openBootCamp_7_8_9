51e79ff7ec679755a4f1ac437da0cad30bc4b786
Norberto Andres Luna <betoluna89@gmail.com>
2023-06-03 19:11:25 -0300

    Agrego los primeros archivos

--- src/com/openbootcamp/Ej_7.java
+++ src/com/openbootcamp/Ej_7.java
@@ -1,5 +1,8 @@
 package com.openbootcamp;
 
+// 7. Crea una función DividePorCero. Esta, debe generar una excepción ("throws") a su llamante del tipo ArithmeticException que será capturada por su llamante (desde "main", por
+// ejemplo). Si se dispara la excepción, mostraremos el mensaje "Esto no puede hacerse". Finalmente, mostraremos en cualquier caso: "Demo de código".
+
 public class Ej_7 {
 
 }
